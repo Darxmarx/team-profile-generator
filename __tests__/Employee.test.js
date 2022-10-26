03859025ea0373b8483d569789aa56ec847a075e
Darx <mcrivabode@gmail.com>
2022-10-25 20:08:23 -0700

    fix tests folder name, create employee.test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,24 @@
+const Employee = require ('./lib/Employee');
+
+// name, email, id, what role
+const employeeTest = new Employee ("Kirby", "kirby@popstar.com", 2, "Employee");
+
+describe('Employee', () => {
+    it('has a name', () => {
+        expect(employeeTest.name).toEqual(expect.any(String)); // should be a string value
+        expect(employeeTest.name.length).toBeGreaterThan(2); // no 2 letter long names
+    })
+
+    it('has an email', () =>{
+        expect(employeeTest.email).toEqual(expect.stringContaining('@')); // all emails have the @
+    })
+
+    it('has a numerical value for an id', () => {
+        expect(employeeTest.id).toEqual(expect.any(Number)); // must be a number
+    })
+
+    it('is an employee', () => {
+        expect(employeeTest.role).toBe('Employee');
+    })
+
+})
